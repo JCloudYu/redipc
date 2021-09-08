11929cc8889a83e42599367fe5dce43a995ea22d
Cloud Yu <j.cloud.yu@gmail.com>
2021-09-08 11:27:10 +0800

    Minor modifications

---
+++ dist/redipc.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="node" />
+import Events = require('events');
+declare type AnyObject = {
+    [key: string]: any;
+};
+declare type AnyFunction = (...args: any[]) => void;
+declare type REDIPCInitOptions = {
+    redis: {
+        uri: string;
+    };
+    channels: string[];
+};
+declare type REDISErrorDescriptor = {
+    code?: string;
+    message?: string;
+} & AnyObject & Error;
+declare type HandlerMap = {
+    [func: string]: AnyFunction;
+};
+export declare class REDISError extends Error {
+    [key: string]: any;
+    readonly code: string;
+    constructor(err_desc: REDISErrorDescriptor);
+}
+export default class REDIPC extends Events.EventEmitter {
+    constructor(init_inst_id?: string);
+    get id(): string;
+    get connected(): boolean;
+    register(func: string | HandlerMap, handler?: AnyFunction): REDIPC;
+    remoteCall(channel: string, func: string, ...args: any[]): Promise<any>;
+    static init(options: REDIPCInitOptions): Promise<REDIPC>;
+}
+export {};
